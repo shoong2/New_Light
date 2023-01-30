e4c31ed7b5e09ddc192d129bd4815c1ec59ff452
Eon_2e <43786927+shoong2@users.noreply.github.com>
2023-01-31 04:57:52 +0900

    0131

--- Light-main/Light/Assets/Script/Battle/GameManager_Battle.cs
+++ Light-main/Light/Assets/Script/Battle/GameManager_Battle.cs
@@ -6,14 +6,29 @@ public class GameManager_Battle : MonoBehaviour
 {
     int monsterCount = 0;
     public GameObject monster;
+    public GameObject monsterHp;
+
+    public List<Transform> obj;
+    public List<GameObject> bar;
+
+    Camera camera;
     void Start()
     {
+        camera = Camera.main;
         monsterCount = Random.Range(1, 6);
 
         for(int i=0;i<monsterCount;i++)
         {
-            monster.transform.GetChild(i).gameObject.SetActive(true);
+            obj[i].gameObject.SetActive(true);
+            //monster.transform.GetChild(i).gameObject.SetActive(true);
+            Instantiate(monsterHp, new Vector2(obj[i].position.x, obj[i].position.y+0.8f), Quaternion.identity,
+                GameObject.Find("Canvas").transform);
         }
+
+        //for(int i=0; i<monsterCount+1; i++)
+        //{
+        //    bar[i].transform.position = obj[i].position + new Vector3(0,1f,0);
+        //}
     }
 
     // Update is called once per frame
